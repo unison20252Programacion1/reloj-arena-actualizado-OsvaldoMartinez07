b9895b61b67b43521d157045a8541dbaeef75014
OsvaldoMartinez07 <OsvaldoDiaz.07@icloud.com>
2025-11-26 18:09:53 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -5,11 +5,20 @@ def reloj_arena(m: int, s: str) -> str:
      if m <= 0:
         print("Error: La altura debe ser un entero positivo")
         return 
-    
-    # TODO: implementar la lógica para generar el reloj de arena en ASCII
-    for i in range(m+1,1):
-        print(s * i)
+ # TODO: implementar la lógica para generar el reloj de arena en ASCII Logica aqui 
 
-    #parte 2: Triangulo Decreciente 
-    for i in range(- 1, 0,m -1):
-        print(s * i)
+    caracter = s[0]
+
+    for i in range(m):
+        num_espacios = i
+        num_caracteres = 2 * (m - i) - 1
+        linea = " " * num_espacios + caracter * num_caracteres
+        print(linea)
+
+    if m > 1:
+        for k in range(1, m):
+            num_espacios = m - 1 - k
+            num_caracteres = 2 * k + 1
+            linea = " " * num_espacios + caracter * num_caracteres
+            print(linea)
+        
