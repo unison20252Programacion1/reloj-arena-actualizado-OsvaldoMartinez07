0ff6e3f53860ed16f6a9e8c8743c089630e9bd5c
OsvaldoMartinez07 <OsvaldoDiaz.07@icloud.com>
2025-11-26 18:16:06 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -5,13 +5,11 @@ def reloj_arena(m: int, s: str) -> str:
         return 
  # TODO: implementar la lógica para generar el reloj de arena en ASCII Logica aqui 
     caracter = s[0]
-    
     for i in range(m):
         num_espacios = i
         num_caracteres = 2 * (m - i) - 1
         linea = " " * num_espacios + caracter * num_caracteres
         print(linea)
-
     if m > 1:
         for k in range(1, m):
             num_espacios = m - 1 - k
